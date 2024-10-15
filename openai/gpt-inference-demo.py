77fa9a7b63a5be3cc6ecb38450fc016c41654f7c
Jeremy Pedersen <jeremydavispedersen@gmail.com>
2024-10-15 13:13:40 +0000

    Added simple demo to call gpt-4o-mini, will expand with more features later

---
+++ openai/gpt-inference-demo.py
@@ -0,0 +1,31 @@
+import gradio as gr
+from openai import OpenAI
+
+def call_gpt(key, prompt):
+
+    client = OpenAI(api_key=key)
+
+    completion = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {
+                "role": "user",
+                "content": prompt
+            }
+        ]
+    )
+
+    return completion.choices[0].message.content # Return first response
+
+demo = gr.Interface(
+    fn=call_gpt,
+    inputs=[gr.Textbox(label="OpenAI API Key"), gr.Textbox(label="Prompt")],
+    outputs=[gr.Textbox(label="Model output", show_copy_button=True)],
+)
+
+# Note: add root_path="/proxy/<port>" to .launch()
+# when running on Coder IDE
+demo.launch(root_path="/proxy/7860")
+# demo.launch() # When running locally
+# demo.launch() # When running locally and sharing globally
